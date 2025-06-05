61804dc615269762bfa4a2ec3f6d3828d494fd08
officerdown <148710565+officerdwn@users.noreply.github.com>
2025-06-05 13:51:51 -0500

    Merge UEFI changes with kernel

--- uefi_main.c
+++ uefi_main.c
@@ -6,6 +6,15 @@ EFI_STATUS
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     InitializeLib(ImageHandle, SystemTable);
+    Print(L"officerdownOS UEFI boot\n");
+    Print(L"Press any key to start the kernel...\n");
+
+    EFI_INPUT_KEY key;
+    uefi_call_wrapper(SystemTable->ConIn->Reset, 2, SystemTable->ConIn, FALSE);
+    while (uefi_call_wrapper(SystemTable->ConIn->ReadKeyStroke, 2,
+                             SystemTable->ConIn, &key) == EFI_NOT_READY) {
+    }
+
     init_uefi(SystemTable);
     kernel_main();
     return EFI_SUCCESS;
